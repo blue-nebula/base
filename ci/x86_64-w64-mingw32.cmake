75948b7b16fd5e1ca1527ae5436559ac2e5404e7
TheAssassin <theassassin@assassinate-you.net>
2020-04-21 14:48:21 +0200

    Make MINGW_PREFIX available for use in CMake

--- ci/x86_64-w64-mingw32.cmake
+++ ci/x86_64-w64-mingw32.cmake
@@ -1,15 +1,15 @@
 # the name of the target operating system
 set(CMAKE_SYSTEM_NAME Windows)
 
-set(PREFIX x86_64-w64-mingw32)
+set(MINGW_PREFIX x86_64-w64-mingw32 CACHE STRING "" FORCE)
 
 # which compilers to use for C and C++
-set(CMAKE_C_COMPILER ${PREFIX}-gcc)
-set(CMAKE_CXX_COMPILER ${PREFIX}-g++)
-set(CMAKE_RC_COMPILER ${PREFIX}-windres)
+set(CMAKE_C_COMPILER ${MINGW_PREFIX}-gcc)
+set(CMAKE_CXX_COMPILER ${MINGW_PREFIX}-g++)
+set(CMAKE_RC_COMPILER ${MINGW_PREFIX}-windres)
 
 # here is the target environment located
-set(CMAKE_FIND_ROOT_PATH /usr/${PREFIX})
+set(CMAKE_FIND_ROOT_PATH /usr/${MINGW_PREFIX})
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
@@ -22,13 +22,13 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(bundled_libs_dir ${PROJECT_SOURCE_DIR}/src/bundled-libs)
 
 set(ZLIB_INCLUDE_DIR ${bundled_libs_dir}/include)
-set(ZLIB_LIBRARY ${bundled_libs_dir}/${PREFIX}/lib/zlib1.dll)
+set(ZLIB_LIBRARY ${bundled_libs_dir}/${MINGW_PREFIX}/lib/zlib1.dll)
 
 set(SDL2_INCLUDE_DIR ${bundled_libs_dir}/include CACHE PATH "" FORCE)
-set(SDL2_LIBRARY ${bundled_libs_dir}/${PREFIX}/lib/SDL2.dll CACHE PATH "" FORCE)
+set(SDL2_LIBRARY ${bundled_libs_dir}/${MINGW_PREFIX}/lib/SDL2.dll CACHE PATH "" FORCE)
 
 set(SDL2_IMAGE_INCLUDE_DIR ${bundled_libs_dir}/include CACHE PATH "" FORCE)
-set(SDL2_IMAGE_LIBRARY ${bundled_libs_dir}/${PREFIX}/lib/SDL2_image.dll CACHE PATH "" FORCE)
+set(SDL2_IMAGE_LIBRARY ${bundled_libs_dir}/${MINGW_PREFIX}/lib/SDL2_image.dll CACHE PATH "" FORCE)
 
 set(SDL2_MIXER_INCLUDE_DIR ${bundled_libs_dir}/include CACHE PATH "" FORCE)
-set(SDL2_MIXER_LIBRARY ${bundled_libs_dir}/${PREFIX}/lib/SDL2_mixer.dll CACHE PATH "" FORCE)
+set(SDL2_MIXER_LIBRARY ${bundled_libs_dir}/${MINGW_PREFIX}/lib/SDL2_mixer.dll CACHE PATH "" FORCE)
