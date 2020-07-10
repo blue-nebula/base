e6caa65fd620260490c6a18f9405190adde0ad56
TheAssassin <theassassin@assassinate-you.net>
2020-07-10 04:35:10 +0200

    Add headers to CMake targets
    This fixes the code inspection in some IDEs, and is generally good
    style, as CMake can then react to changes in these files better.

--- src/cmake/build_client.cmake
+++ src/cmake/build_client.cmake
@@ -39,6 +39,34 @@ if(BUILD_CLIENT)
         engine/water.cpp
         engine/world.cpp
         engine/worldio.cpp
+        engine/animmodel.h
+        engine/bih.h
+        engine/depthfx.h
+        engine/engine.h
+        engine/explosion.h
+        engine/iqm.h
+        engine/irc.h
+        engine/lensflare.h
+        engine/lightmap.h
+        engine/lightning.h
+        engine/md2.h
+        engine/md3.h
+        engine/md5.h
+        engine/model.h
+        engine/mpr.h
+        engine/obj.h
+        engine/octa.h
+        engine/ragdoll.h
+        engine/rendertarget.h
+        engine/scale.h
+        engine/skelmodel.h
+        engine/smd.h
+        engine/sound.h
+        engine/textedit.h
+        engine/texture.h
+        engine/version.h
+        engine/vertmodel.h
+        engine/world.h
         game/ai.cpp
         game/bomber.cpp
         game/capture.cpp
@@ -53,12 +81,40 @@ if(BUILD_CLIENT)
         game/server.cpp
         game/waypoint.cpp
         game/weapons.cpp
+        game/ai.h
+        game/aiman.h
+        game/auth.h
+        game/bomber.h
+        game/bombermode.h
+        game/capture.h
+        game/capturemode.h
+        game/compass.h
+        game/defend.h
+        game/defendmode.h
+        game/duelmut.h
+        game/game.h
+        game/gamemode.h
+        game/player.h
+        game/playerdef.h
+        game/teamdef.h
+        game/vars.h
+        game/weapdef.h
+        game/weapons.h
         shared/crypto.cpp
         shared/geom.cpp
         shared/glemu.cpp
         shared/stream.cpp
         shared/tools.cpp
         shared/zip.cpp
+        shared/command.h
+        shared/cube.h
+        shared/ents.h
+        shared/geom.h
+        shared/glemu.h
+        shared/glexts.h
+        shared/iengine.h
+        shared/igame.h
+        shared/tools.h
     )
 
     # dependencies are imported globally in src/CMakeLists.txt
@@ -69,8 +125,10 @@ if(BUILD_CLIENT)
     if(APPLE)
         # build OS X specific Objective-C code
         set(mac_client_sources
+            xcode/SDLMain.h
             xcode/SDLMain.m
             xcode/macutils.mm
+            xcode/ConsoleView.h
             xcode/ConsoleView.m
         )
         list(APPEND client_sources ${mac_client_sources})
