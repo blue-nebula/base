6a37a94b245b26a47868019cb3d43413d738e85c
voidanix <51296985+voidanix@users.noreply.github.com>
2022-06-10 09:29:06 +0200

    Document what the sound enums actually are

--- src/game/weapons.h
+++ src/game/weapons.h
@@ -28,6 +28,7 @@ enum {
     W_S_EXCLUDE = (1<<W_S_IDLE)|(1<<W_S_POWER)|(1<<W_S_ZOOM)
 };
 
+// sounds emitted by weapons
 enum
 {
     S_W_PRIMARY = 0, S_W_SECONDARY,
@@ -43,6 +44,7 @@ enum
     S_W_MAX
 };
 
+// sounds when picking up weapons
 enum
 {
     S_WEAPONS   = S_GAME,
