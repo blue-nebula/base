8f73ef573c7e03163e9fc28c19265cb1399a4fac
TheAssassin <theassassin@assassinate-you.net>
2025-12-31 22:44:51 +0100

    Bump version to 1.6.1

--- src/engine/version.h
+++ src/engine/version.h
@@ -2,15 +2,15 @@
 #define VERSION_H
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 6
-#define VERSION_PATCH 0
-#define VERSION_STRING "1.6.0"
+#define VERSION_PATCH 1
+#define VERSION_STRING "1.6.1"
 #define VERSION_NAME "Blue Nebula"
 #define VERSION_UNAME "blue-nebula"
 #define VERSION_VNAME "BLUE_NEBULA"
-#define VERSION_RELEASE "Sunset Edition"
+#define VERSION_RELEASE "Silvester Edition"
 #define VERSION_URL "https://blue-nebula.org"
 // TODO: fix copyright note
-#define VERSION_COPY "2009-2020"
+#define VERSION_COPY "2009-2017, 2020-2025"
 #define VERSION_DESC "A fun-filled new take on the first-person arena shooter, featuring a unique parkour system."
 
 #define LAN_PORT 28799
